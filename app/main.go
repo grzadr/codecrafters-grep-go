2cf0456cfdf4d2dac8606bb85f9f150873dc73aa
Adrian Grzemski <adrian.grzemski@gmail.com>
2025-06-29 21:50:04 +0200

    Update VSCode autoSave setting and refactor error handling in main.go

--- app/main.go
+++ app/main.go
@@ -5,18 +5,17 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"unicode"
 	"unicode/utf8"
 )
 
-// Ensures gofmt doesn't remove the "bytes" import above (feel free to remove
-// this!)
-var _ = bytes.ContainsAny
+const errorExitCode = 2
 
 // Usage: echo <input_text> | your_program.sh -E <pattern>.
 func main() {
 	if len(os.Args) < 3 || os.Args[1] != "-E" {
 		fmt.Fprintf(os.Stderr, "usage: mygrep -E <pattern>\n")
-		os.Exit(2) // 1 means no lines were selected, >1 means error
+		os.Exit(errorExitCode) // 1 means no lines were selected, >1 means error
 	}
 
 	pattern := os.Args[2]
@@ -26,34 +25,41 @@ func main() {
 	) // assume we're only dealing with a single line
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "error: read input text: %v\n", err)
-		os.Exit(2)
+		os.Exit(errorExitCode)
 	}
 
 	ok, err := matchLine(line, pattern)
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "error: %v\n", err)
-		os.Exit(2)
+		os.Exit(errorExitCode)
 	}
 
 	if !ok {
 		os.Exit(1)
 	}
-	// default exit code is 0 which means success
+	// default exit code is 0 which means succes
 }
 
-func matchLine(line []byte, pattern string) (bool, error) {
+func matchLine(line []byte, pattern string) (ok bool, err error) {
 	if utf8.RuneCountInString(pattern) != 1 {
-		return false, fmt.Errorf("unsupported pattern: %q", pattern)
+		return ok, fmt.Errorf("unsupported pattern: %q", pattern)
 	}
 
-	var ok bool
+	switch pattern {
+	case `\w`:
+		ok = bytes.ContainsFunc(line, func(r rune) bool {
+			return unicode.IsLetter(r) || unicode.IsDigit(r) || r == '_'
+		})
 
-	// You can use print statements as follows for debugging, they'll be visible
-	// when running tests.
-	fmt.Fprintln(os.Stderr, "Logs from your program will appear here!")
+	case `\d`:
+		ok = bytes.ContainsFunc(line, func(r rune) bool {
+			return unicode.IsNumber(r)
+		})
+	default:
+		ok = bytes.ContainsAny(line, pattern)
+	}
 
 	// Uncomment this to pass the first stage
-	ok = bytes.ContainsAny(line, pattern)
 
 	return ok, nil
 }
